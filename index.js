3a6f7ff09fb335627c737f5a7c822788ce7732f2
C0mbed <matt@neatly.com>
2019-01-03 06:21:36 -0700

    added navigator location services on icon click

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict';
 
+const geoOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+function getGeoSuccess(pos) {
+    console.log(pos);
+    const crd = pos.coords;
+    console.log('Current position: ', crd);
+}
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function watchIcon() {
+    console.log('watchIcon run');
+    $('#location_arrow').click(event => {
+        event.preventDefault();
+        navigator.geolocation.getCurrentPosition(getGeoSuccess, error, geoOptions);
+        console.log('getGeoPos Called');
+    });
+}
+
 function watchForm() {
     console.log('WatchForm run');
     $('#submit_button').click(event => {
@@ -10,3 +35,4 @@ function watchForm() {
   }
 
   $(watchForm);
+  $(watchIcon);
